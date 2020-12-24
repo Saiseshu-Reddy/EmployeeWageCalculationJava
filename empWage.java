86a39a4fbd5200507d61533a518c6e6a5153f75d
Saiseshu Reddy <saiseshureddy.isanaka@gmail.com>
2020-12-24 19:14:51 +0530

    Employee Wage Calculation program

---
+++ empWage.java
@@ -0,0 +1,8 @@
+public class empWage
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Employee Wage Calculation Program on Master Branch");
+	}	//main function
+}	//class
+
