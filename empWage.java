db62cb5d1a960c51d56fc391abc65120f6fa6c72
Saiseshu Reddy <saiseshureddy.isanaka@gmail.com>
2020-12-24 19:21:48 +0530

    Employee Attendance

--- empWage.java
+++ empWage.java
@@ -2,7 +2,13 @@ public class empWage
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Employee Wage Calculation Program on Master Branch");
-	}	//main function
-}	//class
-
+	System.out.println("Welcome to Employee Wage Calculation Program on Master Branch");
+   	int isPresent = 1;
+      	int empCheck =(int)Math.floor(Math.random() * 10) % 2;
+      	System.out.println(empCheck);
+      		if (empCheck == isPresent)
+      		System.out.println("present");
+      		else
+      		System.out.println("absent");
+	}
+}
