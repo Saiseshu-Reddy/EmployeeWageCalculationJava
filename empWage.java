99c4361b2a2c0a10328027b7d33e6d058ac35611
Saiseshu Reddy <saiseshureddy.isanaka@gmail.com>
2020-12-24 19:27:18 +0530

    Daily wage of employee

--- empWage.java
+++ empWage.java
@@ -4,11 +4,16 @@ public class empWage
 	{
 	System.out.println("Welcome to Employee Wage Calculation Program on Master Branch");
    	int isPresent = 1;
-      	int empCheck =(int)Math.floor(Math.random() * 10) % 2;
-      	System.out.println(empCheck);
-      		if (empCheck == isPresent)
-      		System.out.println("present");
-      		else
-      		System.out.println("absent");
+	int empRatePrHr = 20;
+	int empHr = 8;
+    	int empCheck=(int)Math.floor(Math.random() * 10) % 2;
+    	System.out.println(empCheck);
+    	if (empCheck == isPresent) {
+		System.out.println("Employee is Present");
+		int perDaySalary = empRatePrHr * empHr;
+		System.out.println("Employee per day salary = "+perDaySalary);	//Daily wage calculation if employee is present
+	}
+    else
+		System.out.println("Employee is Absent");
 	}
 }
